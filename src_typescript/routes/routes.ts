fb2d0d41652f8ecd1c5ff31b8d9c9839b46e3ccb
EmAS <clemens@huebner-clan.de>
2024-01-03 11:31:43 +0100

    change root route to server info

--- src_typescript/routes/routes.ts
+++ src_typescript/routes/routes.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 
+import {API_VERSION} from "../horror_survey_api_server";
+
 import toObj from "../config/responseStandart";
 
 import apiRoute from "./apiRoute";
@@ -9,7 +11,12 @@ const routes = Router();
 
 routes.use("/api", apiRoute);
 
-routes.use("/", staticRoute);
+//routes.use("/", staticRoute);
+
+routes.get("/", function(request: Request, response: Response) {
+    const welcomeMsg = { API_NAME: process.env.APP_NAME + " API", API_VERSION: API_VERSION};
+    response.status(200).json(toObj(response,welcomeMsg));
+});
 
 routes.use("*", function(request: Request, response: Response) {
     response.status(403).json(toObj(response));
