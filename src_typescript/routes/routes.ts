3d58dffeda364a8e98ace2e5af956f894bb0e824
Clemens Hübner <clemens.huebner@innogames.com>
2024-01-09 21:02:53 +0100

    test

--- src_typescript/routes/routes.ts
+++ src_typescript/routes/routes.ts
@@ -11,15 +11,10 @@ const routes = Router();
 
 routes.use("/api", apiRoute);
 
-//routes.use("/", staticRoute);
-
-routes.get("/", function(request: Request, response: Response) {
-    const welcomeMsg = { API_NAME: process.env.APP_NAME + " API", API_VERSION: API_VERSION};
-    response.status(200).json(toObj(response,welcomeMsg));
-});
+routes.use("/", staticRoute);
 
 routes.use("*", function(request: Request, response: Response) {
-    response.status(404).json(toObj(response));
+    response.status(403).json(toObj(response));
 });
 
 export default routes;
